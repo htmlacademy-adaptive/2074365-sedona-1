cd92885c18e462c6c6cf94f84ec208ff28123e56
Dima <ddd12@mail.ru>
2024-03-16 18:58:34 +0300

    Правки перед защитой №2

---
+++ source/js/map.js
@@ -0,0 +1,5 @@
+let interactiveMap = document.querySelector('.position-on-map__interactive-map');
+let staticMap = document.querySelector('.position-on-map__static-map');
+
+interactiveMap.classList.toggle('position-on-map__no-js');
+staticMap.classList.toggle('position-on-map__no-js');
